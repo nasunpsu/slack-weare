69239abf38bcdd142ad84cfbeb94325c8ee5d201
Matt <matthewrmancini@gmail.com>
2019-01-20 14:41:41 -0500

    cleans ldap code

--- server/ldap.js
+++ server/ldap.js
@@ -1,11 +1,27 @@
+//File used to extract basic ldap data like major from psu's database
+
 const LDAP = require('ldap-client');
+/**
+ * Function to get ldap object used to query psu's ldap database
+ * @returns promise containing ldap object connected and ready to use for psu's ldap database
+ */
 const getLdap = (() => {
+    /** ldap object to return */
     let ldap = null;
     return () => new Promise((resolve, reject) => {
+        //if ldap defined, return it otherwise set it up
         if (!!ldap) {
             resolve(ldap);
             return;
         }
+        const onReady = (error) => {
+            // if there is an error reject the promise
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(ldap);
+        };
         ldap = new LDAP({
             uri: 'ldap://ldap.psu.edu:389',
             validatecert: false,
@@ -14,25 +30,19 @@ const getLdap = (() => {
             attrs: '*',
             filter: '(objectClass=*)',
             scope: LDAP.SUBTREE,
-            connect: () => null,
-            disconnect: () => null,
-        }, (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve(ldap);
-        });
-
+        }, onReady);
     });
 })();
 
+/**
+ * Function that searches the ldap database for a given email
+ * @param {*} email Email of the user to search for
+ * @returns promise containing full ldap data for the user found
+ */
 const searchLdap = async (email) => {
     const ldap = await getLdap();
     console.log(ldap);
     options = {
-        base: 'dc=psu,dc=edu',
-        scope: LDAP.SUBTREE,
         filter: `(mail=${email})`,
         attrs: '*'
     }
@@ -42,10 +52,9 @@ const searchLdap = async (email) => {
                 reject(err);
                 return;
             }
-            resolve(data);
+            resolve(data[0]);
         });
     });
 }
-searchLdap('mrm6089@psu.edu').then(res => {
-    console.log(res);
-});
+
+module.exports = searchLdap;
