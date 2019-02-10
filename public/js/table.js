a25119ac7d4b21d94ae94cbb30743761d730a16d
Matt <matthewrmancini@gmail.com>
2019-02-10 15:17:43 -0500

    search based on name

--- public/js/table.js
+++ public/js/table.js
@@ -1,44 +1,72 @@
-const getBody = () => document.getElementById('table-body');
+class UserTable {
 
-const getHead = () => document.getElementById('table-head');
+    constructor() {
+        this.userElementName = 'element';
+        this.tableBody = document.getElementById('table-body');
+        this.tableHeading = document.getElementById('table-head');
+        this.search = document.getElementById('search');
+        this.headings = (() => {
+            const children = Array.from(this.tableHeading.children);
+            return children.map(child => child.innerText);
+        })();
+        this.users = (() => {
+            const rows = Array.from(this.tableBody.children);
+            return rows.map(tableRow => {
+                const cells = Array.from(tableRow.children); 
+                const user = cells.reduce((user, cell, index) => {
+                    const cellHeading = this.headings[index];
+                    user[cellHeading] = cell.innerText;
+                    return user;
+                }, {});
+                user[this.userElementName] = tableRow;
+                return user;
+            });
+        })();
+        this.originalUsers = [...this.users];
+        this.sortUsers = this.sortUsers.bind(this);
+        this.rerenderUsers = this.rerenderUsers.bind(this);
+        this.searchUsers = this.searchUsers.bind(this);
 
-const getHeadings = () => {
-    const tableHead = getHead();
-    return Array.from(tableHead.children).map(child => child.innerText);
-}
-
-const getUsers = () => {
-    const headings = getHeadings();
-    const tableBody = getBody();
-    return Array.from(tableBody.children).map(tableRow => {
-        const obj = Array.from(tableRow.children).reduce((obj, cell, index) => {
-            const cellHeading = headings[index];
-            obj[cellHeading] = cell.innerText;
-            return obj;
-        }, {});
-        obj['element'] = tableRow;
-        return obj;
-    });
-}
-
-const sortUsers = (fieldName, isReverse) => {
-    let sortedList = getUsers().sort((user1, user2) => {
-        const field1 = user1[fieldName];
-        const field2 = user2[fieldName];
-        return field1.localeCompare(field2);
-    }); 
-    if(isReverse){
-        sortedList = sortedList.reverse();
+        this.search.addEventListener('keydown', () => this.searchUsers(this.search.value));
     }
-    rerenderUsers(sortedList);
-}
 
-const rerenderUsers = (userList) => {
-    const tableBody = getBody();
-    while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild);
+    searchUsers(query){
+        if(query === ''){
+            this.users = [...this.originalUsers];
+            this.rerenderUsers();
+            return;
+        }
+        const results = fuzzysort.go(query, this.originalUsers, {key: this.headings[0]});
+        this.users = results.map(result => result.obj);
+        this.rerenderUsers();
+        const results2 = fuzzysort.go(query, this.users, {keys: this.headings});
+        console.log(results2);
+        // const elements = results.map(result => result.obj.element);
     }
-    userList.forEach((tableRow) => {
-        tableBody.appendChild(tableRow['element']);
-    });
+
+    sortUsers(fieldName, isReverse) {
+        let sortedList = this.users.sort((user1, user2) => {
+            const field1 = user1[fieldName];
+            const field2 = user2[fieldName];
+            return field1.localeCompare(field2);
+        });
+        if (isReverse) {
+            sortedList = sortedList.reverse();
+        }
+        this.users = sortedList;
+        rerenderUsers();
+    }
+
+    rerenderUsers() {
+        while (this.tableBody.firstChild) {
+            this.tableBody.removeChild(this.tableBody.firstChild);
+        }
+        this.users.forEach((user) => {
+            const userElement = user[this.userElementName];
+            this.tableBody.appendChild(userElement);
+        });
+    }
 }
+document.addEventListener("DOMContentLoaded", (event) => {
+    const table = new UserTable();
+});
