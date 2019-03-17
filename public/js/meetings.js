ccaa733646bea5827de3ed754d7b3fe60d70e2aa
snowshine09 <greenwood_sonia@yahoo.com>
2019-03-17 17:52:43 -0400

    table semantic ui sortable; add a placeholder for organizer; add home nav; changed the meeting nav location

--- public/js/meetings.js
+++ public/js/meetings.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", function () {
-
+    $('table').tablesort();
     document.getElementById("new").addEventListener("click", function (e) {
         console.log('save clicked');
         // e.preventDefault();
