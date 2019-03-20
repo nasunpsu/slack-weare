fef3a9e5e8119c7f5b4b7fe83be84cc172e452be
Matt <matthewrmancini@gmail.com>
2019-03-20 10:32:05 -0400

    population of new profile view template

--- bin/weare.js
+++ bin/weare.js
@@ -1207,7 +1207,7 @@ app.get('/profile/:uid', async function (req, res) {
 		console.error(`No results found for uid ${uid}`);
 	}
 	else {
-		to_be_rendered.userInfo = doc[0];
+		to_be_rendered.user = doc[0];
 	}
 	res.render('profileview', to_be_rendered);
 });
