fd1bbb340f5f3a181610dd6f3589cb88f61326b6
snowshine09 <greenwood_sonia@yahoo.com>
2019-03-31 14:30:11 -0400

    check the member left condition to see if the user exists

--- bin/weare.js
+++ bin/weare.js
@@ -883,7 +883,7 @@ app.post('/slack/events', (req, res, next) => {
 							console.log(`the event body is ${util.inspect(event, { depth: null })}`);
 							DB.collection('users').findOne({ uid: team + '_' + user }, function (err, tobeDEL) {
 								if (err) console.error(err);
-								else {
+								else if(tobeDEL){
 									if (tobeDEL.channels.length == 1) { //this will be the last channel that the user is leaving, meaning that he/she is being deactivating
 										DB.collection('users_deactivated').updateOne({ uid: team + '_' + user }, {
 											$set: {
